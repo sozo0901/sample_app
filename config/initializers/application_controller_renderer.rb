4041bbc2ed58d43727909a13f01bc88143004152
EC2 Default User <ec2-user@ip-172-31-3-32.ec2.internal>
2021-09-06 07:40:20 +0000

    [Finish] 2-7 attachment_image_tagを埋め込む

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6955ad1c48c57caf760a4d2f88c752bea3c58f47e91e8b9d34e0f2eea1a711177c84845080d50b1534fc090bd34d80306bf4e7dcc842498a499983e637b89a8e'
